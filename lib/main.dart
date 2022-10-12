486312cc0b33b2765801b88f008381c1e39e485b
ehilding <erik.hildingsson@gmail.com>
2022-10-12 09:49:20 +0200

    test

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ import 'presentation/screens/home_page.dart';
 import 'presentation/screens/login_page.dart';
 
 // ...
+// .....
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
